4a023f1ab83e4ee62de04ee9d397bcf797782524
Andrew Storch <astorch13@gmail.com>
2020-11-10 20:04:12 -0500

    over wrote devise registration controller to allow for first name and lst name fields

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :passwords
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root 'static#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
